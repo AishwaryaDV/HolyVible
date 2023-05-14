ca703d0194d82fb5ef3fb89d9f3697f6caa888c6
AishwaryaDV <millamatew@gmail.com>
2023-05-14 18:26:49 +0530

    landing page header

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,13 +6,17 @@ const Landing = (props) => {
     const navigate= useNavigate();
 
   return (
+    <>
     <div className="landing-page-section">
+      <button className="signup-btn">Sign up</button>
+      <button className="login-btn">Log in</button>
     <h1>Holy Vible</h1>
     <h2 className="header-tagline">
         <span>Let the</span> good vibes <span>roll</span>
     </h2>
     <button className="landing-page-btn" onClick={()=>navigate("/home")}>Let's get started</button>
     </div>
+    </>
   )
 }
 
